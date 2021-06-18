13ce3bb56890c9338c2c18f8b312e249a146de78
brayaneider1 <brayaneider1@gmail.com>
2021-06-18 15:38:06 -0500

    first commit

---
+++ src/components/Layout/LayoutPublic/LayoutPublic.js
@@ -0,0 +1,15 @@
+import React from "react"
+import MenuImg from '../../../assets/images/Icons/menu.svg'
+
+
+const LayoutPublic = () => {
+  return (
+    <>
+    <div className="menu">
+      <img src={MenuImg} alt="menu"/>
+    </div>
+    </>
+  )
+}
+
+export default LayoutPublic
---
+++ src/pages/home/index.js
@@ -0,0 +1,50 @@
+import React from "react"
+import $ from 'jquery';
+
+
+import Menu from "../../components/Layout/LayoutPublic/LayoutPublic";
+
+import tasksImg from '../../assets/images/Background/tasks.svg'
+import responsiveImg from '../../assets/images/Background/responsive.svg'
+import pcImg from '../../assets/images/Background/pc.svg'
+import silueta from '../../assets/images/Background/SiluetaYisus.svg'
+
+const Home = () => {
+
+  $(document).ready(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 0) {
+        $(".section1").css({ "opacity": "0" })
+      }
+      else {
+        $(".section1").css({ "opacity": "1" })
+      }
+      if ($(this).scrollTop() > 0) {
+        $(".section2").css({ "opacity": "1" })
+      }
+      else {
+        $(".section2").css({ "opacity": "0" })
+      }
+    })
+  })
+  return (
+    <div className="home">
+      <Menu />
+      
+      <div className="container">
+        <div className="section1">
+          <img src={tasksImg} alt="task" />
+          <img src={responsiveImg} alt="responsive" />
+          <img className="computer" src={pcImg} alt="computer" />
+          <span className="primary-title">¡PLEASE SCROLL! </span>
+        </div>
+
+      </div>
+      <div className="section2">
+        <h3 >WEB DESIGNER </h3>
+        <img src={silueta} alt="silueta" />
+      </div>
+    </div>
+  )
+}
+export default Home
